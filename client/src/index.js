92eb4e0a8761267e78345fda693687bde4d9e334
N R <ramoskel343@gmail.com>
2023-02-11 23:54:57 -0700

    root added for latest react version

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from "react-router-dom"
-import App from './App';
-import { PawsContextProvider } from "./PawsContext"
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import App from "./App";
+import { PawsContextProvider } from "./PawsContext";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-ReactDOM.render(
- 
-     <Router>
-         <PawsContextProvider>
-               <App />
-         </PawsContextProvider>
-     </Router>
-,
-  document.getElementById('root')
-  )
-
-
-
-
-
-
-
-
+root.render(
+  <Router>
+    <PawsContextProvider>
+      <App />
+    </PawsContextProvider>
+  </Router>,
+  document.getElementById("root")
+);
