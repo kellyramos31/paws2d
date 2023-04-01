4d97c0074966407f6ae5e5f9da00b19b045628ad
N R <ramoskel343@gmail.com>
2023-04-01 16:33:01 -0600

    update includes to look for object prop

--- client/src/components/PawsContext.js
+++ client/src/components/PawsContext.js
@@ -95,7 +95,7 @@ class PawsContextProvider extends Component {
     let addedProperty = this.state.dogFriendlyRestaurants.map((restaurant) => {
       console.log("restaurant.id", restaurant.id)
       console.log("this.state.myFaves", this.state.myFaves)
-      this.state.myFaves.includes(restaurant.id) ? console.log("myFaves isHearted true") : console.log("myFaves isHearted false");
+      this.state.myFaves.includes({id: restaurant.id}) ? console.log("myFaves isHearted true") : console.log("myFaves isHearted false");
       restaurant.myDoggieImage = "";
       return restaurant;
     });
